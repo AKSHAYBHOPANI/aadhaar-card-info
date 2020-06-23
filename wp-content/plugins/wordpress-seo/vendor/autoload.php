fd74f27b7684373c178358bc673964e22e71557a
Akshay Bhopani <akshaybhopani@gmail.com>
2020-06-23 09:29:43 +0530

    Finalising

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5a7dd57aa1001ba4ffc487e5b8c22e4b::getLoader();
+return ComposerAutoloaderInit26c34ec75d0e59fca5276a1ce5534878::getLoader();
